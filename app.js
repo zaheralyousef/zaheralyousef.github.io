832ea97baf7a46f6454ebe33363262e5dd18c7ca
Zaher Alyousef <zaheralyousef@gmail.com>
2016-10-15 00:27:14 +0200

    saving the last work

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBSwT1VSDoz0NQjviRJXd82EiQvBXE870Y",
+	authDomain: "alsirafy-booking.firebaseapp.com",
+	databaseURL: "https://alsirafy-booking.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "220338051474"
+};
+firebase.initializeApp(config);
 
 // Declare app level module which depends on views, and components
 angular.module('aBookingApp', [
 	'ngRoute',
+	'firebase',
 	'aBookingApp.booking',
 	'aBookingApp.alsirafy',
 	'aBookingApp.about',
--- booking/booking.js
+++ booking/booking.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('aBookingApp.booking', ['ngRoute'])
+angular.module('aBookingApp.booking', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/booking', {
@@ -9,6 +9,75 @@ angular.module('aBookingApp.booking', ['ngRoute'])
 	});
 }])
 
-.controller('BookingCtrl', [function() {
+.controller('BookingCtrl', ['$scope', '$firebaseArray', '$firebaseObject', function($scope, $firebaseArray, $firebaseObject) {
+	const ref = firebase.database().ref().child('passengers');
+	$scope.passengers = $firebaseArray(ref);
+
+	//Show Add Form
+	$scope.showAddForm = function() {
+		$scope.addFormShow = true;
+	}
+
+
+	// Hide
+	$scope.hide = function() {
+		$scope.addFormShow = false;
+		$scope.passengerShow = false;
+	}
+
+	// Add Form Submit
+	$scope.addFormSubmit = function() {
+		var name = $scope.name ? $scope.name : null;
+		var email = $scope.email ? $scope.email : null;
+		var company = $scope.company ? $scope.company : null;
+		var mobile_phone = $scope.mobile_phone ? $scope.company : null;
+		var home_phone = $scope.home_phone ? $scope.home_phone : null;
+		var work_phone = $scope.work_phone ? $scope.work_phone : null;
+		var street_address = $scope.street_address ? $scope.street_address : null;
+		var city = $scope.city ? $scope.city : null;
+		var state = $scope.state ? $scope.state : null;
+		var zipcode = $scope.zipcode ? $scope.zipcode : null;
+
+		//build object
+		$scope.passengers.$add({
+			name: name,
+			email: email,
+			company: company,
+			phones: [{
+				mobile_phone: mobile_phone,
+				home_phone: home_phone,
+				work_phone: work_phone
+			}],
+			adress: [{
+				street_address: street_address,
+				city: city,
+				state: state,
+				zipcode: zipcode
+			}]
+		}).then(function(ref) {
+			var id = ref.key;
+
+			//clear form 
+			clearFields();
+			$scope.addFormShow = false;
+			$scope.msg = 'Contact Added';
+		});
+
+
+	}
+
+		// Clear $scope Fields
+	function clearFields() {
+		$scope.name = '';
+		$scope.email = '';
+		$scope.company = '';
+		$scope.mobile_phone = '';
+		$scope.home_phone = '';
+		$scope.work_phone = '';
+		$scope.street_address = '';
+		$scope.city = '';
+		$scope.state = '';
+		$scope.zipcode = '';
+	}
 
 }]);
